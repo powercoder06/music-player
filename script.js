6397b97aca79cf009fdd7a142ceb53c1de18a3d6
powercoder06 <ayush.kumar2021c@vitstudent.ac.in>
2022-07-16 18:48:43 +0530

    Template Music player finished

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -89,6 +93,50 @@ function nextSong() {
 // on load- Select first song
 loadSong(songs[songIndex]);
 
+//Update Progress Bar & Time
+function updateProgressBar(e) {
+    if(isPlaying){
+        const {duration, currentTime } =e.srcElement;
+        //Update progress Bar width
+        const progressPercent = (currentTime/ duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        //Calculate display for duration
+        const durationMinutes = Math.floor(duration/60);
+      
+        let durationSeconds = Math.floor(duration % 60);
+        if(durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`; 
+        }
+       
+        //Delay switching duration Element to avoid NaN
+        if(durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        //Calculate display for currentTime
+        const currentMinutes = Math.floor(currentTime/60);
+       
+        let currentSeconds = Math.floor(currentTime % 60);
+        if(currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`; 
+        }
+        //Delay switching currentTime
+        if(currentSeconds) {
+            currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+        }
+    }
+}
+
+//Set progress Bar
+function setProgressBar(e) {
+   const width = this.clientWidth;
+   const clickX = e.offsetX;
+   const {duration} =music;
+   music.currentTime = (clickX / width) * duration;
+}
+
 //Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
